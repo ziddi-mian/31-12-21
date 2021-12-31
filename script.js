fbb994abd5882a42a6099b12eb6718ed57a1f60c
Ziddi-Mian <ahmadyounas893@gmail.com>
2021-12-31 15:30:24 +0500

    js

--- script.js
+++ script.js
@@ -287,3 +287,18 @@ function startOfWeek(date)
 dt = new Date(); 
 
 console.log(startOfWeek(dt).toString());
+
+
+// Write a JavaScript function to get the week start date.  
+
+function startOfWeek(date)
+  {
+    var diff = date.getDate() - date.getDay() + (date.getDay() === 0 ? -6 : 1);
+  
+    return new Date(date.setDate(diff));
+ 
+  }
+
+dt = new Date(); 
+
+console.log(startOfWeek(dt).toString());
