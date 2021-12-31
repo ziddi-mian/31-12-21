da556dd05c4734f74e199e68a53b000973dbc4c2
Ziddi-Mian <ahmadyounas893@gmail.com>
2021-12-31 15:31:46 +0500

    js

--- script.js
+++ script.js
@@ -339,3 +339,18 @@ switch (true) {
         console.log("Enter valid operation.");
         break;
 }
+
+
+
+// Write a JavaScript function to get the month end date.  
+
+function endOfMonth(date)
+  {
+     
+  return new Date(date.getFullYear(), date.getMonth() + 1, 0);
+ 
+  }
+
+dt = new Date(); 
+
+console.log(endOfMonth(dt).toString());
